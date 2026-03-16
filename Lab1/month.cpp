d27794f60f2b7a2dc7b692b4e0d7470bc4ceb218
Shahnewaz Azad <shahnewazazad@shahnewazs-air.mynetworksettings.com>
2026-03-16 12:36:47 -0400

    Lab1D

---
+++ Lab1/month.cpp
@@ -0,0 +1,49 @@
+/*
+Author: Shahnewaz Azad
+Course: CSCI-136
+Assignment: Lab1D
+
+C++ project that prints the days in a month, taking into account if it isa leap year or not
+*/
+
+#include <iostream>
+
+std::string leapyear(int n){
+    if(n%4 != 0){
+        return "Common year";
+    }
+    else if(n%100 != 0){
+        return "Leap year";
+    }
+    else if(n%400 != 0){
+        return "Common year";
+    }
+    else{
+        return "Leap year";
+
+    }
+
+    return 0;
+}
+
+int main(){
+    int year, month;
+    std::cout << "Enter year: ";
+    std::cin >> year;
+    std::cout << "Enter month: ";
+    std::cin >> month;
+    if(month == 1||month == 3||month == 5 ||month == 7||month == 8||month == 10||month == 12){
+        std::cout << "31 days\n";
+    }
+    else if(month == 4||month == 6||month == 9||month == 11){
+        std::cout << "30 days\n";
+    }
+    else{
+        if(leapyear(year) == "Leap year"){
+            std::cout << "29 days\n";
+        }
+        else{
+            std::cout << "28 days\n";
+        }
+    }
+}
